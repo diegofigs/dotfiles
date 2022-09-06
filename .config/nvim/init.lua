8ecaf1be3e1e06b176bfb8d033d6b68469e93294
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-09-06 19:30:17 -0400

    refactor: move to complete lua config, abstract keymap functions to module

---
+++ .config/nvim/init.lua
@@ -0,0 +1,28 @@
+local Plug = vim.fn["plug#"]
+
+vim.call("plug#begin")
+
+Plug("nvim-lua/plenary.nvim")
+Plug("jose-elias-alvarez/null-ls.nvim")
+Plug("nvim-treesitter/nvim-treesitter")
+Plug("nvim-telescope/telescope.nvim")
+Plug("neovim/nvim-lspconfig")
+Plug("glepnir/lspsaga.nvim", { branch = "main" })
+Plug("hrsh7th/cmp-nvim-lsp")
+Plug("hrsh7th/cmp-buffer")
+Plug("hrsh7th/cmp-path")
+Plug("hrsh7th/cmp-cmdline")
+Plug("hrsh7th/nvim-cmp")
+Plug("hrsh7th/cmp-vsnip")
+Plug("hrsh7th/vim-vsnip")
+Plug("nvim-lualine/lualine.nvim")
+Plug("kyazdani42/nvim-web-devicons")
+Plug("kyazdani42/nvim-tree.lua")
+Plug("mhinz/vim-startify")
+Plug("morhetz/gruvbox")
+
+vim.call("plug#end")
+
+vim.cmd("colorscheme gruvbox")
+
+require("diegofigs")
--- .config/nvim/lua/diegofigs/nvim-treesitter.lua
+++ .config/nvim/lua/diegofigs/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all"
-	ensure_installed = { "typescript", "tsx", "javascript", "lua" },
+	ensure_installed = { "javascript", "typescript", "tsx", "lua" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
