b4d4d96ac3789e5bb2fbd0765a55e060a81b6d1f
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-09-10 15:06:14 -0400

    refactor: move rust/cargo related data, add cat alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,7 +1,9 @@
 # aliases
 alias nf='neofetch'
 alias vim='nvim'
+alias cat='bat'
 alias ls='exa'
 alias l='ls -l'
 alias ll='ls -la'
 alias grep='grep -n --color'
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -18,3 +18,6 @@ export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
 
 export TERMINFO="$XDG_DATA_HOME"/terminfo
 export TERMINFO_DIRS="$XDG_DATA_HOME"/terminfo:/usr/share/terminfo
+
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+export CARGO_HOME="$XDG_DATA_HOME"/cargo
