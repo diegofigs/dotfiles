edff9a4f9815d35dc72f87f49267606fc7492a44
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-09-15 22:01:56 -0400

    refactor: move from vim-plug to packer, move vim options to core

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -47,5 +47,8 @@ vim.opt.ttyfast = true
 vim.g.mapleader = ";"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
