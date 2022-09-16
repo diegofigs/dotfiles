1ae91397dc2e0c94e4677ce0129931c6234a6444
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-09-16 00:17:12 -0400

    feat: add packer bootstrap for new installs

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local packer = require("packer")
 
 return packer.startup(function(use)
@@ -59,4 +72,9 @@ return packer.startup(function(use)
 	})
 	use({ "mhinz/vim-startify" })
 	use({ "EdenEast/nightfox.nvim", tag = "v1.0.0" })
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
