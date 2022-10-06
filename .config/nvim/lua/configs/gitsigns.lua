d17bc870cec21fcc2eb3511eed7a656b35e49a17
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-10-06 14:47:29 -0400

    feat: add gitsigns config including keymaps

---
+++ .config/nvim/lua/configs/gitsigns.lua
@@ -0,0 +1,58 @@
+local Remap = require("core.keymap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+
+require("gitsigns").setup({
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		nnoremap("]c", function()
+			if vim.wo.diff then
+				return "]c"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		nnoremap("[c", function()
+			if vim.wo.diff then
+				return "[c"
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		-- Actions
+    nnoremap("<leader>hs", ":Gitsigns stage_hunk<CR>")
+		vnoremap("<leader>hs", ":Gitsigns stage_hunk<CR>")
+		nnoremap("<leader>hr", ":Gitsigns reset_hunk<CR>")
+		vnoremap("<leader>hr", ":Gitsigns reset_hunk<CR>")
+		nnoremap("<leader>hS", gs.stage_buffer)
+		nnoremap("<leader>hu", gs.undo_stage_hunk)
+		nnoremap("<leader>hR", gs.reset_buffer)
+		nnoremap("<leader>hp", gs.preview_hunk)
+		nnoremap("<leader>hb", function()
+			gs.blame_line({ full = true })
+		end)
+		nnoremap("<leader>tb", gs.toggle_current_line_blame)
+		nnoremap("<leader>hd", gs.diffthis)
+		nnoremap("<leader>hD", function()
+			gs.diffthis("~")
+		end)
+		nnoremap("<leader>td", gs.toggle_deleted)
+
+		-- Text object
+		--map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+	end,
+})
