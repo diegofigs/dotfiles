0a5e94e258b7e3adc183fda27604fed9446d32ae
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-11-13 13:49:09 -0400

    chore: avoid nix lang warnings

--- .config/zsh/exports.sh
+++ .config/zsh/exports.sh
@@ -6,7 +6,6 @@ export HISTSIZE=30000;
 export SAVEHIST=24000;
 
 export LANG='en_US.UTF-8';
-export LC_ALL='en_US.UTF-8';
 
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}"/asdf/asdfrc
 export ASDF_DATA_DIR="${XDG_DATA_HOME}"/asdf
