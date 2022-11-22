5c4e5cd46d411a63ef07aa3efe7c8f9ba851e135
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-11-22 15:04:25 -0400

    feat: add lsp list to lualine

--- .config/nvim/lua/configs/lualine.lua
+++ .config/nvim/lua/configs/lualine.lua
@@ -1,3 +1,25 @@
+local function lsp_server_name()
+	local client_names = {}
+	local nb_clients = 0
+	local msg = "No Active Lsp"
+	local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+	local clients = vim.lsp.get_active_clients()
+	if next(clients) == nil then
+		return msg
+	end
+	for _, client in ipairs(clients) do
+		local filetypes = client.config.filetypes
+		if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+			table.insert(client_names, client.name)
+			nb_clients = nb_clients + 1
+		end
+	end
+	if nb_clients > 0 then
+		return table.concat(client_names, ",")
+	end
+	return msg
+end
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
@@ -19,8 +41,28 @@ require("lualine").setup({
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_b = {
+			{
+				"filename",
+				file_status = true,
+				symbols = { modified = " ", readonly = " " },
+			},
+		},
+		lualine_c = {
+			{ "branch", icon = "" },
+			{ "diff" },
+			{
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+				symbols = { error = " ", warn = " ", info = " " },
+			},
+			{
+				lsp_server_name,
+				icon = " LSP:",
+				padding = 15,
+				color = { fg = "#7195e1", gui = "bold" },
+			},
+		},
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
