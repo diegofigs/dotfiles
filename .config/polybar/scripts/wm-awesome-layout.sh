4a9c13ee63128d0a698ff89ee5acf7eddcae2aa7
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-01-06 17:26:42 -0400

    style: lint polybar scripts

--- .config/polybar/scripts/wm-awesome-layout.sh
+++ .config/polybar/scripts/wm-awesome-layout.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 layout_toggle() {
-    $(awesome-client 'require("awful").layout.inc(1, client.screen);')
+    awesome-client 'require("awful").layout.inc(1, client.screen);'
 }
 
 current_layout() {
     STATUS=$(awesome-client 'return mouse.screen.selected_tag.layout.name')
-    echo $STATUS | cut -d' ' -f2 | sed 's/"//g'
+    echo "$STATUS" | cut -d' ' -f2 | sed 's/"//g'
 }
 
 case "$1" in
