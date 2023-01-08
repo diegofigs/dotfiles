0eb9680a8d808f4664ee3a15f6f85935057a44a1
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-01-08 01:02:28 -0400

    style: lint nvim config

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -8,8 +8,8 @@ require("telescope").setup({
 				-- actions.which_key shows the mappings for your picker,
 				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
 				["<C-h>"] = "which_key",
-                ['<C-u>'] = false,
-                ['<C-d>'] = false,
+				["<C-u>"] = false,
+				["<C-d>"] = false,
 			},
 		},
 	},
