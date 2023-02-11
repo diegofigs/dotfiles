ad7f9a0706be4adb21bc2d8e59b92520a8164eb1
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-02-11 11:08:25 -0400

    refactor: use programmatic tagnames for default theme

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -133,7 +133,7 @@ local mytextclock = wibox.widget.textclock()
 
 function theme.at_screen_connect(s)
 	-- Each screen has its own tag table.
-	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+	awful.tag(awful.util.tagnames, s, awful.layout.layouts[1])
 
 	-- Create a promptbox for each screen
 	s.mypromptbox = awful.widget.prompt()
