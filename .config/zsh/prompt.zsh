ef624074cf03f3c9614b9e7b676f19e4f71bd913
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-10-24 11:20:33 -0400

    chore: use rtx in both linux and mac environments

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -7,16 +7,15 @@ if [ "$(uname -s)" = "Darwin" ]; then
     source "$BREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
     # source $BREW_PREFIX/opt/powerlevel10k/powerlevel10k.zsh-theme
 
-    # Activate rtx
-    eval "$(rtx activate zsh)"
 else
     source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
     # source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
-
-    # Initialize asdf
-    . /opt/asdf-vm/asdf.sh
 fi
+
+# Activate rtx
+eval "$(rtx activate zsh)"
+
 # source $ZDOTDIR/.p10k.zsh
 
 eval "$(starship init zsh)"
