755e28a979bf8b773a7fb1de1f8b7a634f1bc015
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-10-24 12:55:10 -0400

    chore: remove more exports

--- .config/zsh/exports.sh
+++ .config/zsh/exports.sh
@@ -13,6 +13,8 @@ export PATH="$HOME/bin:$PATH";
 export MANPAGER="less -X -R --use-color -Dd+r -Du+b"
 export LANG='en_US.UTF-8';
 
+export PF_INFO="ascii title os wm shell editor uptime pkgs memory palette"
+
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}"/asdf/asdfrc
 export ASDF_DATA_DIR="${XDG_DATA_HOME}"/asdf
 
@@ -39,20 +41,15 @@ if [ "$(uname -s)" = "Darwin" ]; then
     export PATH="$BREW_PREFIX/opt/gnu-tar/libexec/gnubin:$PATH"
     export PATH="$BREW_PREFIX/opt/grep/libexec/gnubin:$PATH"
     export PATH="$BREW_PREFIX/opt/curl/bin:$PATH"
-
-    USER_BASE_BIN=$(python3 -m site --user-base)/bin
-    export PATH="$USER_BASE_BIN:$PATH"
 else
     export ANDROID_HOME=~/Android/Sdk
 fi
 
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/build-tools/33.0.0
+export PATH="$ANDROID_HOME/emulator:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/build-tools/33.0.0:$PATH"
 
 export PATH="$XDG_DATA_HOME/solana/install/active_release/bin:$PATH"
 export PATH="$XDG_DATA_HOME/cargo/bin:$PATH"
 export PATH="$HOME/.avm/bin:$PATH"
 export PATH="$HOME/.config/.foundry/bin:$PATH"
-
-export PF_INFO="ascii title os wm shell editor uptime pkgs memory palette"
